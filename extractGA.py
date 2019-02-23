b6557243ca630eb66934cc92f3a8aeaccebfe15f
fjblau <fjblau@gmail.com>
2019-02-23 16:03:44 +0100

    Working version

--- extractGA.py
+++ extractGA.py
@@ -4,6 +4,8 @@ from apiclient.discovery import build
 from oauth2client.service_account import ServiceAccountCredentials
 import json
 import mysql.connector as mysql
+from mysql.connector import Error
+from datetime import datetime
 
 
 SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
@@ -26,25 +28,8 @@ def initialize_analyticsreporting():
   return analytics
 
 
-def initialize_mysql():
-
-  db = mysql.connect(
-    host = "localhost",
-    user = "wx",
-    passwd = "wx"
-    )
-  cursor = db.cursor()
-  print("Database Open")
-  return cursor
-
 def get_report(analytics): 
-  """Queries the Analytics Reporting API V4.
-
-  Args:
-    analytics: An authorized Analytics Reporting API V4 service object.
-  Returns:
-    The Analytics Reporting API V4 response.
-  """
+ 
   return analytics.reports().batchGet(
       body={
         'reportRequests': [
@@ -52,41 +37,39 @@ def get_report(analytics):
           'viewId': VIEW_ID,
           'dateRanges': [{'startDate': '3daysAgo', 'endDate': 'today'}],
           'metrics': [ {'expression': 'ga:pageviews'} ],
-          'dimensions': [{'name': 'ga:channelGrouping'}, {'name': 'ga:date'}]
+          'dimensions': [{'name': 'ga:source'}, {'name': 'ga:date'}]
         }]
       }
   ).execute()
 
 
-def print_response(response):
+def insert_response(response):
   rowcount = int(json.dumps(response['reports'][0]['data']['rowCount']))
-  print (rowcount)
   r = json.dumps(response['reports'][0]['data']['rows'])
- 
+  print(r)
+  db = mysql.connect(user='wx', password='wx', host='localhost', port='3306', database='ga') 
+  if db.is_connected:
+    print("DB isConnected")
+  cursor = db.cursor()
+  
+
   metric = json.loads(r)
   for i in range (0, rowcount):
+
     source = json.dumps(metric[i]['dimensions'][0]).replace('"', '')
     date = json.dumps(metric[i]['dimensions'][1]).replace('"', '')
     m = json.dumps(metric[i]['metrics'][0]['values'][0]).replace('"', '')
-    #print (int(m.replace('"','')))
-    print(source, date, int(m))
-
-  #output = json.loads(r['reports'][0])
-  #pj = json.dumps(output, indent=4, sort_keys=True)
-  #print (pj)
-
-  """Parses and prints the Analytics Reporting API V4 response.
-
-  Args:
-    response: An Analytics Reporting API V4 response.
-  """
-
+    mysqldate  = datetime.strptime(date, '%Y%m%d').date()
+    sql = "insert into ga(ga_date, source, metric) values(%s, %s, %s)"
+    val = (mysqldate, source, int(m))
+    cursor.execute(sql, val)
+    db.commit()
+    
 
 def main():
-  database = initialize_mysql()
   analytics = initialize_analyticsreporting()
   response = get_report(analytics)
-  print_response(response)
+  insert_response(response)
 
 if __name__ == '__main__':
   main()
